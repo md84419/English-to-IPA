01f1c1aec6e04cbeead700179f8bd256722626d0
Michael Davey <michael.davey@robotica.ml>
2020-08-16 14:29:09 +0000

    Workspace snapshot

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup
 
-with open("README.md", 'r') as f:
+with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(
-    name='English to IPA',
-    version='0.2.1',
-    description='take English text and convert it to IPA',
-    author=['mphilli', 'Mitchellpkt', 'CanadianCommander', 'timvancann'],
+    name="English-to-IPA",
+    version="0.3.0-alpha.1",
+    description="Take English text and convert it to IPA",
+    description_content_type="text/plain",
+    author=["mphilli", "Mitchellpkt", "CanadianCommander", "timvancann", "md84419"],
     long_description=long_description,
+    long_description_content_type="text/markdown",
     include_package_data=True,
-    packages=['eng_to_ipa'],
+    packages=["eng_to_ipa"],
 )
