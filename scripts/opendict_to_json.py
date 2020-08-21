b65beae1d2781122fd81d5e0eeef11db5e6e6a04
Michael Davey <michael.davey@robotica.ml>
2020-08-21 12:24:04 +0000

    Mark the diphthongs; Map phonetic to phonemic; don't use tri- and quad-

--- scripts/opendict_to_json.py
+++ scripts/opendict_to_json.py
@@ -3,7 +3,7 @@
 # USAGE:
 #  PYTHONPATH=".." python opendict_to_json.py ../eng_to_ipa/resources/Opendict_source_files/en_UK.txt > ../eng_to_ipa/resources/Open_dict.json
 
-import getopt, json, io, os, sys, subprocess
+import getopt, json, io, os, re, sys, subprocess
 from signal import signal, SIGPIPE, SIG_DFL
 from eng_to_ipa import tokenize
 
@@ -48,14 +48,12 @@ def main(argv):
   
 
   p_out = subprocess.check_output( ['awk', '-f', 'txt_to_json.awk', input_file] )
-  
+
   open_dict = json.loads( p_out )
+  fix_opendict_words( open_dict )
+
   for key in open_dict:
     #if debug2: print( open_dict[key][0] )
-    #print ( tokenize( open_dict[key][0] ) )
-    #mylist.print()
-    if( len( open_dict[key]) > 1 ):
-      print( key )
     for idx in range( len( open_dict[key] ) ):
       open_dict[key][idx] = fix_opendict( open_dict[key][idx] )
       open_dict[key][idx] = tokenize.tokenize( open_dict[key][idx] )
@@ -84,10 +82,61 @@ def fix_opendict(source):
   destination = destination.replace("ɛː", "eə")
   destination = destination.replace("ɛ", "e")
 
+  #mark the diphthongs with the non-breaking space character
+  destination = destination.replace("aɪ", "a‍ɪ") # 39
+  destination = destination.replace("aʊ", "a‍ʊ") # 45
+  destination = destination.replace("eə", "e‍ə") # 42
+  destination = destination.replace("eɪ", "e‍ɪ") # 38
+  destination = destination.replace("ɔɪ", "ɔ‍ɪ") # 40
+  destination = destination.replace("əʊ", "ə‍ʊ") # 45
+  destination = destination.replace("ɛə", "ɛ‍ə") # 42
+  destination = destination.replace("ɪə", "ɪ‍ə") # 41
+  destination = destination.replace("ʊə", "ʊ‍ə") # 43
+  
+  # mark the affricates with the non-breaking space character
+  destination = destination.replace("dʒ", "d‍ʒ")
+  destination = destination.replace("tʃ", "t‍ʃ")
+  
+  # mark US English diphthong /ie/ - we'll undo this again in create_en_GB_dict
+  destination = destination.replace("iə", "i‍ə")
+  
+  # unmark US English /əl/
+  destination = destination.replace("ə‍l", "əl")
+
+  # Use the standard (quantitative-qualitative) IPA notation scheme for vowels
+  # See section 5 of https://www.phon.ucl.ac.uk/home/wells/ipa-english-uni.htm
+  destination = re.sub("ɑ(?!‍)", "ɑː", destination)
+  destination = destination.replace("ɒː", "ɒ")
+  destination = re.sub("i(?!‍)", "iː", destination)
+  destination = re.sub("ɔ(?!‍)", "ɔː", destination)
+  destination = re.sub("u(?!‍)", "uː", destination)
+  destination = destination.replace("ːː", "ː")
+
+  # Replace US diphthongs with UK diphthongs in UK dictionary
+  destination = destination.replace("ɔ‍ɪ‍ə‍ʊ", "ɔ‍ɪə‍ʊ")
+  destination = destination.replace("ɔ‍ɪ‍ə‍ʊ", "ɔ‍ɪə‍ʊ")
+  destination = destination.replace("e‍ɪ‍ə‍ʊ", "e‍ɪə‍ʊ")
+  destination = destination.replace("a‍ɪ‍ə‍ʊ", "a‍ɪə‍ʊ")
+
+  #Split triphthongs into diphthong followed by schwa or ʊ
+  #'a‍ɪ‍ə', 'a‍ʊ‍ə', 'e‍ɪ‍ə', 'e‍ə‍ʊ', 'ɔ‍ɪ‍ə', 'ɪ‍ə‍ʊ', 'ə‍ʊ‍ə'
+  destination = destination.replace("a‍ɪ‍ə", "a‍ɪə")
+  destination = destination.replace("a‍ʊ‍ə", "a‍ʊə")
+  destination = destination.replace("e‍ɪ‍ə", "e‍ɪə")
+  destination = destination.replace("e‍ə‍ʊ", "e‍əʊ")
+  destination = destination.replace("ɔ‍ɪ‍ə", "ɔ‍ɪə")
+  destination = destination.replace("ɪ‍ə‍ʊ", "ɪ‍əʊ")
+  destination = destination.replace("ə‍ʊ‍ə", "ə‍ʊə")
+
   # Map phonetic to phonemic
   destination = destination.replace("ɐ", "ʌ")
   destination = destination.replace("ɡ", "g")
   return destination
-      
+
+# fix whole words
+def fix_opendict_words( dct ):
+  dct.update({"ablation": ["ʌblˈe‍ɪʃən","ʌblˈe‍ɪʃn"]})
+  #dct.update({"abdominal": ["æbdˈɒmənəl"]})
+
 if( __name__ == "__main__"):
   main(sys.argv[1:])
