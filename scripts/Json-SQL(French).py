f8204baca67778dd3154cd4026ae47ae31b2cd57
Matt-JL-Burton <mat@emux.app>
2021-08-19 12:52:56 +0100

    database finished (IPA not yet normalsied)

--- scripts/Json-SQL(French).py
+++ scripts/Json-SQL(French).py
@@ -7,22 +7,35 @@ import pathlib
 from sqlite3.dbapi2 import connect
 import json
 
+def create_dictionary_table():
+    try:
+        cursor.execute("""CREATE TABLE fr_ipa
+                    (word_id INTEGER PRIMARY KEY,
+                    word text NOT NULL,
+                    ipa text NOT NULL
+                    )""")
+        connection.commit()
+    except sqlite3.OperationalError:
+        cursor.execute("DROP TABLE fr_ipa;")
+        connection.commit()
+        create_dictionary_table()
+
 os.chdir(pathlib.Path(__file__).parent.absolute().parent.absolute())
 os.chdir('.\\eng_to_ipa\\resources')
-with open('fr_Open_Dict.json', 'w',encoding='UTF-8') as fp:
+with open('fr_Open_Dict.json', 'r',encoding='UTF-8') as fp:
     jsonText = json.load(fp)
-    print(jsonText)
-
+    jsonText = dict(jsonText)
+    global words, IPApronociation
+    words = list(jsonText.keys())
+    IPApronociation = list(jsonText.values())
 
 os.chdir(pathlib.Path(__file__).parent.absolute().parent.absolute())
 os.chdir('.\\eng_to_ipa\\resources')
-print(os.getcwd())
 connection = sqlite3.connect("fr_FR.db")
 cursor = connection.cursor()
-
-
-
-
-
+create_dictionary_table()
+for i in range(len(words)):
+    listToAdd = [i+1,words[i],str(IPApronociation[i])]
+    cursor.execute('INSERT INTO fr_ipa (word_id,word,ipa) VALUES (?,?,?)',listToAdd)
 connection.commit()
 connection.close()
